7c3956ac07ddf8eda6ea359188078883fb489d10
Nick Peddie <nfpeddie@gmail.com>
2020-04-03 14:41:02 -0700

    initial setup commit

---
+++ src/pages/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import L from 'leaflet';
+import Axios from 'axios';
+
+import Layout from 'components/Layout';
+import Container from 'components/Container';
+import Map from 'components/Map';
+
+const LOCATION = {
+  lat: 38.9072,
+  lng: -77.0369
+};
+const CENTER = [LOCATION.lat, LOCATION.lng];
+const DEFAULT_ZOOM = 2;
+
+const IndexPage = () => {
+
+  /**
+   * mapEffect
+   * @description Fires a callback once the page renders
+   * @example Here this is and example of being used to zoom in and set a popup on load
+   */
+
+  async function mapEffect({ leafletElement: map } = {}) {
+    if ( !map ) return;
+
+    let response;
+
+    try {
+      response = await Axios.get('https://corona.lmao.ninja/countries');
+
+    } catch(e) {
+      console.log('E', e);
+      return;
+    }
+
+    const { data } = response;
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    if ( !hasData ) return;
+
+    const geoJson = {
+      type: 'FeatureCollection',
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          trype: 'Feature',
+          properties: {
+            ...country,
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [ lng, lat ]
+          }
+        }
+      })
+    }
+  }
+
+  const mapSettings = {
+    center: CENTER,
+    defaultBaseMap: 'OpenStreetMap',
+    zoom: DEFAULT_ZOOM,
+    mapEffect
+  };
+
+  return (
+    <Layout pageName="home">
+      <Helmet>
+        <title>Home Page</title>
+      </Helmet>
+
+      <Map {...mapSettings} />
+
+      <Container type="content" className="text-center home-start">
+        <h2>Still Getting Started?</h2>
+        <p>Run the following in your terminal!</p>
+        <pre>
+          <code>gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-leaflet</code>
+        </pre>
+        <p className="note">Note: Gatsby CLI required globally for the above command</p>
+      </Container>
+    </Layout>
+  );
+};
+
+export default IndexPage;
